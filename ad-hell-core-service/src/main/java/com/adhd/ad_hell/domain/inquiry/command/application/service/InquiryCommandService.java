305d4c99de3e0e3eca3e579dc00e53d527b05152
ChAnGMiNBae <qockdals752@gmail.com>
2025-11-07 11:58:58 +0900

    commit

--- ad-hell-core-service/src/main/java/com/adhd/ad_hell/domain/inquiry/command/application/service/InquiryCommandService.java
+++ ad-hell-core-service/src/main/java/com/adhd/ad_hell/domain/inquiry/command/application/service/InquiryCommandService.java
@@ -1,7 +1,6 @@
 package com.adhd.ad_hell.domain.inquiry.command.application.service;
 
 
-import com.adhd.ad_hell.common.util.SecurityUtil;
 import com.adhd.ad_hell.domain.category.command.domain.aggregate.Category;
 import com.adhd.ad_hell.domain.category.command.domain.repository.CategoryRepository;
 import com.adhd.ad_hell.domain.inquiry.command.application.dto.request.InquiryAnswerRequest;
@@ -9,7 +8,6 @@ import com.adhd.ad_hell.domain.inquiry.command.application.dto.request.InquiryAn
 import com.adhd.ad_hell.domain.inquiry.command.domain.repository.InquiryRepository;
 import com.adhd.ad_hell.domain.user.command.entity.User;
 import com.adhd.ad_hell.domain.user.command.repository.UserCommandRepository;
-import com.adhd.ad_hell.domain.user.query.service.provider.UserProvider;
 import com.adhd.ad_hell.exception.BusinessException;
 import com.adhd.ad_hell.exception.ErrorCode;
 import com.adhd.ad_hell.domain.inquiry.command.application.dto.request.InquiryCreateRequest;
@@ -31,21 +29,14 @@ public class InquiryCommandService {
 
     private final InquiryRepository inquiryRepository;
     private final CategoryRepository categoryRepository;
-<<<<<<< HEAD:ad-hell-core-service/src/main/java/com/adhd/ad_hell/domain/inquiry/command/application/service/InquiryCommandService.java
     private final UserCommandRepository userCommandRepository;
     private final NotificationInternalClient notificationClient;
-=======
-    private final SecurityUtil securityUtil;
-    private final UserProvider userProvider;
->>>>>>> develop:ad-hell/src/main/java/com/adhd/ad_hell/domain/inquiry/command/application/service/InquiryCommandService.java
 
     /** 문의 등록 (Builder 사용) */
     @Transactional
     public Long createInquiry(InquiryCreateRequest req) {
-//        User user = userCommandRepository.findById(req.getUserId())
-//                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
-        Long userId = securityUtil.getLoginUserInfo().getUserId();
-        User user = userProvider.getUserById(userId);
+        User user = userCommandRepository.findById(req.getUserId())
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
 
         Category category = categoryRepository.findById(req.getCategoryId())
                 .orElseThrow(() -> new BusinessException(ErrorCode.CATEGORY_NOT_FOUND));
